d31419ce4b3b77454d8e264213e6097812cf9f2c
01aaa01 <abdunnurmusoxonov213@gmail.com>
2025-03-13 22:12:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let string = prompt(`String typedagi ma'lumot kiriting`)
+let number = prompt(`Number typedagi ma'lumot kiriting`)
+let boolean = prompt(`Boolean typedagi ma'lumot kiriting`)
+alert(`Hamma ma'lumotlarni to'g'ri kiritganingizga ishonchingiz komilmi`)
+
+console.log(string);
+console.log(number);
+console.log(boolean);
+
